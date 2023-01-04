9fd25f00de9ccfb77b3412cef2befbbc8b8ff086
Jessa P. Arguilles <116412092+Jessmark-11@users.noreply.github.com>
2023-01-03 22:03:13 -1100

    Update Main.java
    Answer Number 4

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
-
+public class Main{
+  public static void main(String[]args){
+    Customer customer;
+    RegularCustomer
+  regularCustomer=new
+  RegularCustomer("jessmark",1000);
+    SeniorCustomer
+  seniorCustomer=new
+  SeniorCustomer("jess",1000);
+    
+    customer=regularCustomer;
+    
+  System.out.println(customer.getName()+""+customer.calculateBill();
+                     
+     customer=seniorCustomer;
+  System.out.println(customer.getName()+""+customer.calculateBil());
+  }
+}                  
