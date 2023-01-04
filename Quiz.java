2c050f73a014aac8c1303166973c5f7083ad16ae
Jessa P. Arguilles <116412092+Jessmark-11@users.noreply.github.com>
2023-01-03 23:05:28 -1100

    Update Quiz.java
    Answer Number 4

--- Quiz.java
+++ Quiz.java
@@ -1,1 +1,18 @@
-
+public class Quiz{
+  //Data Members
+  private String Id int;
+  private String total score:int;
+  
+  //Constructor
+  public Quiz(){
+    totalscore="int';
+      maxscore="int";
+  }
+  //Returns the maxscore of this Quiz
+  public String getGrade(){
+    returns maxscore
+  }
+  //Returns the double totalscore of this Quiz
+  public String getTotalscore(){
+    return totalscore;
+}    
